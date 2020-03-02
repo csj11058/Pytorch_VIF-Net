5873a9ae6db04f2178c49eb820e6a6c5b4dee6e6
Change ZH <46740575+ChangeZH@users.noreply.github.com>
2020-03-02 15:38:36 +0800

    Update train.py

--- train.py
+++ train.py
@@ -32,11 +32,11 @@ def train():
 	_,_,traindata=load_train_data('./data/TNO/',1,1)
 	# model=torch.load('./model/model300.pth')
 	opt=torch.optim.Adam(model.parameters(),lr)
-	with tqdm(total=epoch) as train_bar:
-		for i in range(1,epoch+1):
-			allloss=0
-			train_bar.update(1)
+	for i in range(1,epoch+1):
+		allloss=0
+		with tqdm(total=math.ceil(traindata/batch_size)) as train_bar:
 			for j in range(0,math.ceil(traindata/batch_size)):
+				train_bar.update(1)
 				input1,input2,_=load_train_data('./data/TNO/',j,batch_size)
 				output=model(input1,input2)
 				loss = criterion(input1,input2,output)
@@ -48,8 +48,8 @@ def train():
 				if i//800-i/800==0:
 					lr=lr/10
 					opt=torch.optim.Adam(model.parameters(),lr)
-			train_bar.set_description('epoch:%s loss:%.5f'%(i,allloss))
-			torch.save(model,'./model/model'+str(i)+'.pth')
+				train_bar.set_description('epoch:%s loss:%.5f'%(i,allloss/(j+1)))
+		torch.save(model,'./model/model'+str(i)+'.pth')
 
 if __name__=='__main__':
 	train()
